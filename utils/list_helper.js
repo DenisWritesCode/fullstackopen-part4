d3c7d8c1b834312db5544afe394df3fa9946dae5
DenisWritesCode <DWC@Denis-MBP.local>
2023-11-04 12:55:31 +0300

    done with 4.4.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1;
 };
 
+// return total sum of likes.
+const totalLikes = (blogs) => {
+  return blogs.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
