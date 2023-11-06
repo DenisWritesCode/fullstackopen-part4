00394e8e4688ca64321558754acc0740a2355a68
DenisWritesCode <DWC@Denis-MBP.local>
2023-11-06 11:57:58 +0300

    added try/catch

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,24 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
-  response.json(blogs);
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", async (request, response, next) => {
   const blog = new Blog(request.body);
 
-  const saveResult = await blog.save();
-  response.status(201).json(saveResult);
+  try {
+    const saveResult = await blog.save();
+    response.status(201).json(saveResult);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = blogsRouter;
