c3f72f0db0065c85f2aae6d864843d3fc8bc8b0d
DenisWritesCode <DWC@Denis-MBP.local>
2023-11-07 11:07:31 +0300

    done with 4.9

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,12 +1,12 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", async (request, response, next) => {
+blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
 
   const saveResult = await blog.save();
--- tests/blog_list.test.js
+++ tests/blog_list.test.js
@@ -11,6 +11,12 @@ describe("controller tests", () => {
       .expect(200)
       .expect("Content-type", /application\/json/);
   });
+
+  test("The unique identifier for blog posts is named id", async () => {
+    const blogs = await api.get("/api/blogs");
+
+    expect(blogs.body[0].id).toBeDefined();
+  });
 });
 
 afterAll(async () => {
