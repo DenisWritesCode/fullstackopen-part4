cdb2063cb5caf7d95f9aed30d32b1db7d0cf13a3
DenisWritesCode <DWC@Denis-MBP.local>
2023-11-07 10:25:27 +0300

    done with 4.8

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const config = require("./utils/config");
 const express = require("express");
-require("express-async-error");
+require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
